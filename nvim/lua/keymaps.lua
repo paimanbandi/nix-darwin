1d4184112233fe97161b7da497d1a6aaaf738200
Paiman <paiman@MacBook-Pro-de-Paiman.local>
2025-07-31 14:03:47 +0700

    keymap for back to buffer

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -6,4 +6,6 @@ k.set('i', 'HH', '<ESC>')
 k.set('v', 'J', ":m '>+1<CR>gv=gv")
 k.set('v', 'K', ":m '<-2<CR>gv=gv")
 
+k.set('n', '<leader><leader>', ':b#<CR>', { silent = true })
+
 k.set("n", "<leader>dl", vim.diagnostic.setloclist, { desc = "Show diagnostics in loclist" })
