70f3ae51fbe18a5998f55d8e664404554e852a84
Paiman <paiman@MacBook-Pro-de-Paiman.local>
2025-08-01 08:17:34 +0700

    automatically change dos file format to unix

--- nvim/lua/autocmd.lua
+++ nvim/lua/autocmd.lua
@@ -1,3 +1,11 @@
+vim.api.nvim_create_autocmd("BufReadPost", {
+  callback = function()
+    if vim.bo.fileformat == "dos" then
+      vim.bo.fileformat = "unix"
+    end
+  end,
+})
+
 vim.api.nvim_create_autocmd("CursorHold", {
   callback = function()
     vim.diagnostic.open_float(nil, {
