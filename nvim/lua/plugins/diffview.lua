9b8bbd5ffe9b43461b73ed916c88c4af1fe829b5
Paiman <paiman.bandi@gmail.com>
2025-11-17 20:18:12 +0700

    update diffview

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -1,3 +1,4 @@
+-- ~/.config/nvim/lua/plugins/diffview.lua
 return {
   "sindrets/diffview.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
@@ -6,10 +7,11 @@ return {
     { "<leader>gd", "<cmd>DiffviewOpen<cr>",        desc = "Open Diffview" },
     { "<leader>gh", "<cmd>DiffviewFileHistory<cr>", desc = "File History" },
     { "<leader>gc", "<cmd>DiffviewClose<cr>",       desc = "Close Diffview" },
-    -- Untuk conflict resolution
-    { "<leader>gm", "<cmd>DiffviewOpen HEAD<cr>",   desc = "Merge Conflicts" },
+    { "<leader>gm", "<cmd>DiffviewOpen<cr>",        desc = "Show Conflicts" },
   },
   config = function()
+    local actions = require("diffview.actions")
+
     require("diffview").setup({
       enhanced_diff_hl = true,
       use_icons = true,
@@ -27,6 +29,7 @@ return {
           layout = "diff2_horizontal",
         },
         merge_tool = {
+          -- Layout untuk conflict resolution
           layout = "diff3_horizontal",
           disable_diagnostics = true,
         },
@@ -43,17 +46,35 @@ return {
         },
       },
       keymaps = {
+        -- Keymaps untuk diff view (saat lihat file)
         view = {
-          { "n", "q",          "<cmd>DiffviewClose<cr>",                { desc = "Close diffview" } },
-          { "n", "<leader>co", "<cmd>DiffviewConflictChooseOurs<cr>",   { desc = "Choose Ours" } },
-          { "n", "<leader>ct", "<cmd>DiffviewConflictChooseTheirs<cr>", { desc = "Choose Theirs" } },
-          { "n", "<leader>cb", "<cmd>DiffviewConflictChooseBase<cr>",   { desc = "Choose Base" } },
-          { "n", "<leader>ca", "<cmd>DiffviewConflictChooseAll<cr>",    { desc = "Choose All" } },
-          { "n", "dx",         "<cmd>DiffviewConflictDelete<cr>",       { desc = "Delete conflict region" } },
+          { "n", "q",          actions.close,                     { desc = "Close diffview" } },
+          { "n", "<tab>",      actions.select_next_entry,         { desc = "Next file" } },
+          { "n", "<s-tab>",    actions.select_prev_entry,         { desc = "Previous file" } },
+          { "n", "[x",         actions.prev_conflict,             { desc = "Previous conflict" } },
+          { "n", "]x",         actions.next_conflict,             { desc = "Next conflict" } },
+          -- Commands untuk resolve conflict (hanya work saat ada conflict)
+          { "n", "<leader>co", actions.conflict_choose("ours"),   { desc = "Choose Ours" } },
+          { "n", "<leader>ct", actions.conflict_choose("theirs"), { desc = "Choose Theirs" } },
+          { "n", "<leader>cb", actions.conflict_choose("base"),   { desc = "Choose Base" } },
+          { "n", "<leader>ca", actions.conflict_choose("all"),    { desc = "Choose All" } },
+          { "n", "dx",         actions.conflict_choose("none"),   { desc = "Delete conflict region" } },
         },
+        -- Keymaps untuk file panel (sidebar)
         file_panel = {
-          { "n", "q",    "<cmd>DiffviewClose<cr>",                                    { desc = "Close diffview" } },
-          { "n", "<cr>", "<cmd>lua require('diffview.actions').goto_file_edit()<cr>", { desc = "Open file" } },
+          { "n", "j",       actions.next_entry,        { desc = "Next file" } },
+          { "n", "k",       actions.prev_entry,        { desc = "Previous file" } },
+          { "n", "<cr>",    actions.select_entry,      { desc = "Open file" } },
+          { "n", "o",       actions.select_entry,      { desc = "Open file" } },
+          { "n", "q",       actions.close,             { desc = "Close diffview" } },
+          { "n", "R",       actions.refresh_files,     { desc = "Refresh files" } },
+          { "n", "<tab>",   actions.select_next_entry, { desc = "Next file" } },
+          { "n", "<s-tab>", actions.select_prev_entry, { desc = "Previous file" } },
+        },
+        file_history_panel = {
+          { "n", "q",    actions.close,        { desc = "Close" } },
+          { "n", "o",    actions.select_entry, { desc = "Open" } },
+          { "n", "<cr>", actions.select_entry, { desc = "Open" } },
         },
       },
     })
