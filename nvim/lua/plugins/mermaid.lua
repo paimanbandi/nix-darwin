b1c60f86c717a9898790c8f4507fde92da2afae7
Paiman <paiman.bandi@gmail.com>
2025-11-19 14:59:03 +0700

    update mermaid

--- nvim/lua/plugins/mermaid.lua
+++ nvim/lua/plugins/mermaid.lua
@@ -17,9 +17,59 @@ return {
           end
         }
       })
-      vim.keymap.set("n", "<leader>mp", ":MarkdownPreview<CR>", { desc = "Mermaid/Markdown Preview" })
-      vim.keymap.set("n", "<leader>ms", ":MarkdownPreviewStop<CR>", { desc = "Stop Mermaid Preview" })
-      vim.keymap.set("n", "<leader>mt", ":MarkdownPreviewToggle<CR>", { desc = "Toggle Mermaid Preview" })
+    end
+  },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons"
+    },
+    opts = {
+      code = {
+        sign = false,
+        width = 'block',
+        right_pad = 1,
+      },
+      heading = {
+        sign = false,
+        icons = {}
+      },
+    },
+    ft = { "markdown", "mermaid" },
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    keys = {
+      { "<leader>mp", "<cmd>MarkdownPreview<cr>",       desc = "Mermaid/Markdown Preview" },
+      { "<leader>ms", "<cmd>MarkdownPreviewStop<cr>",   desc = "Stop Mermaid Preview" },
+      { "<leader>mt", "<cmd>MarkdownPreviewToggle<cr>", desc = "Toggle Mermaid Preview" },
+    },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+    ft = { "markdown", "mermaid" },
+    config = function()
+      vim.g.mkdp_theme = 'dark'
+      vim.g.mkdp_auto_start = 0
+      vim.g.mkdp_auto_close = 1
+      vim.g.mkdp_refresh_slow = 0
+      vim.g.mkdp_command_for_global = 0
+      vim.g.mkdp_open_to_the_world = 0
+      vim.g.mkdp_browser = ''
+      vim.g.mkdp_echo_preview_url = 1
+      vim.g.mkdp_preview_options = {
+        mkit = {},
+        katex = {},
+        uml = {},
+        maid = {},
+        disable_sync_scroll = 0,
+        sync_scroll_type = 'middle',
+        hide_yaml_meta = 1,
+        sequence_diagrams = {},
+        flowchart_diagrams = {},
+        content_editable = false,
+      }
     end
   }
 }
