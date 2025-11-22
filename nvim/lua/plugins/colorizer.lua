de514125993af52cc200f5ab88f3528bbc319c96
Paiman <paiman.bandi@gmail.com>
2025-11-22 21:25:37 +0700

    update colorizer plugin

--- nvim/lua/plugins/colorizer.lua
+++ nvim/lua/plugins/colorizer.lua
@@ -3,8 +3,8 @@ return {
   event = { "BufReadPost", "BufNewFile" },
   config = function()
     require("colorizer").setup({
-      "*", -- Highlight all files
-      css = { rgb_fn = true }, -- Enable parsing rgb(...) functions in css
+      "*",                      -- Highlight all files
+      css = { rgb_fn = true },  -- Enable parsing rgb(...) functions in css
       html = { names = false }, -- Disable parsing "names" like Blue or Gray
     })
   end,
