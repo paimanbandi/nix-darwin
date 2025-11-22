78ab32e99bdc700705d5e862517f6e0ce1bf9eab
Paiman <paiman.bandi@gmail.com>
2025-11-22 20:02:21 +0700

    fix undefined global variable vim

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -60,11 +60,18 @@ return {
             on_attach = on_attach,
             settings = {
               Lua = {
+                runtime = {
+                  version = "LuaJIT",
+                },
                 diagnostics = {
                   globals = { "vim" },
                 },
                 workspace = {
+                  library = vim.api.nvim_get_runtime_file("", true),
                   checkThirdParty = false,
+                },
+                telemetry = {
+                  enable = false,
                 },
               },
             },
