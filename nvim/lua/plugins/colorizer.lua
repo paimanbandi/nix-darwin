87e317eb78a048e35b7cda5be38703464f1fb5b6
Paiman <paiman.bandi@gmail.com>
2025-11-22 21:06:44 +0700

    update colorizer

--- nvim/lua/plugins/colorizer.lua
+++ nvim/lua/plugins/colorizer.lua
@@ -1,1 +1,11 @@
-return { 'norcalli/nvim-colorizer.lua' }
+return {
+  "brenoprata10/nvim-highlight-colors",
+  event = { "BufReadPost", "BufNewFile" },
+  config = function()
+    require("nvim-highlight-colors").setup({
+      render = "background", -- 'background' | 'foreground' | 'virtual'
+      enable_named_colors = true,
+      enable_tailwind = true,
+    })
+  end,
+}
