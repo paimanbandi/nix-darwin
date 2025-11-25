6b6613deb09ea5a95f289fb77310d455d92086f5
Paiman <paiman.bandi@gmail.com>
2025-11-25 15:43:55 +0700

    fix - auto move to the aerial window

--- nvim/lua/plugins/aerial.lua
+++ nvim/lua/plugins/aerial.lua
@@ -2,24 +2,36 @@ return {
   "stevearc/aerial.nvim",
   opts = {
     backends = { "lsp", "treesitter", "markdown", "man" },
-    attach_mode = "window",        -- Ubah dari "global" ke "window"
+    attach_mode = "window",
     layout = {
-      default_direction = "float", -- Ubah dari "right" ke "float"
+      default_direction = "float",
       max_width = { 40, 0.2 },
     },
     show_guides = true,
+    -- Tambahan: auto focus saat dibuka
+    on_attach = function(bufnr)
+      vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+      vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+    end,
   },
   keys = {
     {
       "<leader>a",
       function()
         require("aerial").toggle()
-        -- Tunggu sedikit lalu pindah jendela biar fokus ke Aerial
+        -- Delay kecil untuk memastikan window sudah terbuka
         vim.defer_fn(function()
-          vim.cmd("wincmd w") -- sama seperti Ctrl+w w
-        end, 10)
+          -- Cari window aerial dan focus ke sana
+          for _, win in ipairs(vim.api.nvim_list_wins()) do
+            local buf = vim.api.nvim_win_get_buf(win)
+            if vim.bo[buf].filetype == "aerial" then
+              vim.api.nvim_set_current_win(win)
+              break
+            end
+          end
+        end, 50)
       end,
-      desc = "Toggle Outline (Aerial) + Focus",
+      desc = "Toggle Outline (Aerial)"
     },
     { "{", "<cmd>AerialPrev<CR>", desc = "Previous Symbol" },
     { "}", "<cmd>AerialNext<CR>", desc = "Next Symbol" },
