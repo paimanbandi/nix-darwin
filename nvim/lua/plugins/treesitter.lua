2b5b61acc2a1c901b4e45429bc2c56b2c63333d7
Paiman <paiman.bandi@gmail.com>
2025-11-26 18:51:35 +0700

    add markdown for treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup({
-      ensure_installed = { "dart", "rust", "lua", "mermaid" },
+      ensure_installed = { "dart", "rust", "lua", "mermaid", "markdown", "markdown_inline" },
       highlight = { enable = true },
     })
   end,
