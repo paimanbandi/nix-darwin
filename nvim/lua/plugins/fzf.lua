618ffd9677d32e51f25cf23b6a0affcb4a52fe6e
Paiman <paiman.bandi@gmail.com>
2025-11-26 16:26:06 +0700

    wrap-around fzf-lua

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -2,7 +2,11 @@ return {
   "ibhagwan/fzf-lua",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    require("fzf-lua").setup({})
+    require("fzf-lua").setup({
+      fzf_opts = {
+        ["--cycle"] = true,
+      },
+    })
   end,
   keys = {
     { "<leader>ff", "<cmd>FzfLua files<cr>",     desc = "Find Files" },
