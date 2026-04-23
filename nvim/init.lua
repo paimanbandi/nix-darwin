65d2b858d22a40db568e88b9eda04047e0bcd6e7
Paiman <paiman.bandi@gmail.com>
2026-04-23 20:36:13 +0700

    update init

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,16 @@
+-- Workaround nvim 0.12 treesitter.get_range bug
+-- https://github.com/neovim/neovim/issues/39032
+local orig_get_range = vim.treesitter.get_range
+vim.treesitter.get_range = function(node, ...)
+  if node == nil then
+    return { 0, 0, 0, 0, 0, 0 }
+  end
+  local ok, result = pcall(orig_get_range, node, ...)
+  if ok then
+    return result
+  end
+  return { 0, 0, 0, 0, 0, 0 }
+end
 require("bootstrap.lazy")
 require("autocmd")
 require("settings")
